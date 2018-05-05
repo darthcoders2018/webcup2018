cc9cd263bfeb5e009240665ce5e46f348f1d1590
Unknown <darthcoders2018@gmail.com>
2018-05-05 18:21:34 +0400

    #10
    register firebase,error display

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
-function login()
-{
-	  const email = document.getElementById("signin-email").value,
+function login() {
+    const email = document.getElementById("signin-email").value,
         password = document.getElementById("signin-password").value;
 
-         firebase.auth().signInWithEmailAndPassword(email, password)
-        .then(function (response) {
+    firebase.auth().signInWithEmailAndPassword(email, password)
+        .then(function(response) {
             console.log(response.email);
         })
-        .catch(function (error) {
-        	document.getElementById("error_display").innerHTML=error;
+        .catch(function(error) {
+            document.getElementById("error_display").innerHTML = error;
             console.log('error SignIn: ' + error);
+        });
+}
+
+function register() {
+    document.getElementById("error_display").innerHTML = " ";
+    const email = document.getElementById("signin-email").value,
+        password = document.getElementById("signin-password").value;
+
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+        .then(function(response) {
+            debugger
+            console.log("response SignUp: ", response);
+        })
+        .catch(function(error) {
+            document.getElementById("error_display").innerHTML = error;
+            console.log("error SignUp: ", error)
         });
 }
